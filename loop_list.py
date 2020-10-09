2f473b6d3fb1c66315b5cf268f458bb5dc19c5be
Brandon Rhodes <brhodes1984@gmail.com>
2020-10-09 08:22:30 -0500

    chapter 4 lesson 1 & 2 for loop

---
+++ loop_list.py
@@ -0,0 +1,11 @@
+#4-1 Pizzas
+pizzas = ['cheese', 'sausage', 'pepperoni', 'vegan']
+for pizza in pizzas:
+    print(f'I love getting {pizza} pizza from Beggers\n')
+print('I really love pizza!!\n')
+
+#4-2 Animals
+animals = ['cat', 'dog', 'snake', 'fish']
+for pet in animals:
+    print(f'I would love to have a {pet} as a pet!\n')
+print('I would take any of these as a pet and love them.')
